8b6f6971b55f155ad936e32541ababb4c6756713
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2024-02-08 19:42:44 +0100

    fix: remove unrequired dependencies with deprecated dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,13 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
     "airbnb",
     "airbnb-typescript",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "react", "@typescript-eslint", "prettier"],
+  plugins: ["react-refresh", "react", "prettier"],
   rules: {
     "max-len": ["error", 140],
     quotes: [2, "single", { avoidEscape: true }],
