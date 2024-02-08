5b87cc5a7f6aa2350878afbbb6ba5770de7066fd
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2024-02-08 08:12:55 +0100

    feat: eslint + husky setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,21 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "react", "@typescript-eslint", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "max-len": ["error", 140],
+    quotes: [2, "single", { avoidEscape: true }],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
